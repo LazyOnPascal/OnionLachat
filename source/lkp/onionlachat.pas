6c7f2d5eeb00a8a8dc230a5fc167381303193950
LazyOnPascal <LazyOnPascal@proton.me>
2023-01-11 02:09:44 +0400

    Change name of .lpk

---
+++ source/lkp/onionlachat.pas
@@ -0,0 +1,24 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit OnionLachat;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  TorLauncher, ChatProtocol, Bridges, ChatConnection, CommonFunctions, 
+  ChatFunctions, ChatMessage, ChatUser, ChatMessageList, ChatContactList, 
+  ChatContact, ChatConnectionList, ChatServer, ConnectionStreamList, 
+  ConnectionStream, ChatTypes, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('OnionLachat', @Register);
+end.
